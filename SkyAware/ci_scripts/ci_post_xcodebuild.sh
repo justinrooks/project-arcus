ab719b9ef12f7f5385adf1d1fce55599ac4ac143
JRooks <viper1221@me.com>
2026-01-24 10:25:45 -0700

    - Add path safety to TestFlight WhatToTest

--- SkyAware/ci_scripts/ci_post_xcodebuild.sh
+++ SkyAware/ci_scripts/ci_post_xcodebuild.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 #  ci_post_xcodebuild.sh
 
-
-if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
-  TESTFLIGHT_DIR_PATH=../TestFlight
-  mkdir $TESTFLIGHT_DIR_PATH
-  git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-fi
+#if [[ "$CI_XCODEBUILD_ACTION" == "archive" ]]; then
+  if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+    TESTFLIGHT_DIR_PATH="$CI_PROJECT_FILE_PATH/../TestFlight"
+    mkdir $TESTFLIGHT_DIR_PATH
+    git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+  fi
+#fi
